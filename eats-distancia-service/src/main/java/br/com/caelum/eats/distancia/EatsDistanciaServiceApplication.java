cfb57918389489ef6498b2f1da166c073f79d576
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 16:47:49 -0300

    Exercícios 9.1 e 9.2 Eureka Server e Self Registration de Distância

--- eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/EatsDistanciaServiceApplication.java
+++ eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/EatsDistanciaServiceApplication.java
@@ -2,7 +2,9 @@ package br.com.caelum.eats.distancia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EatsDistanciaServiceApplication {
 
