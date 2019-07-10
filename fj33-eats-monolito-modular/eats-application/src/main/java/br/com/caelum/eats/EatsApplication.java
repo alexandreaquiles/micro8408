c5f7f93bd59d2833b763ed67bdabb7907c9511d7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 17:02:51 -0300

    Exercício 9.4 Self Registration do monolito

--- fj33-eats-monolito-modular/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
+++ fj33-eats-monolito-modular/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
@@ -2,7 +2,9 @@ package br.com.caelum.eats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EatsApplication {
 
