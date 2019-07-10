4c8f9d47f6d685237ecebd3b2b9826c5fbbc9ca7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 15:36:46 -0300

    Exercício 8.3: Client Side Load Balancing com Ribbon no API Gateway

--- api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
+++ api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
@@ -11,7 +11,7 @@ public class DistanciaRestClient {
 	private String distanciaServiceUrl;
 
 	public DistanciaRestClient(RestTemplate restTemplate, 
-				@Value("${zuul.routes.distancia.url}") String distanciaServiceUrl) {
+				@Value("${configuracao.distancia.service.url}") String distanciaServiceUrl) {
 		this.restTemplate = restTemplate;
 		this.distanciaServiceUrl = distanciaServiceUrl;
 	}
