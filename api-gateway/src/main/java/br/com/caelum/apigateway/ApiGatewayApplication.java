8e6e002e10fbdf641e886afad904ddf5076b5ef8
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 17:06:31 -0300

    Exercício 9.5 Self Registration do API Gateway

--- api-gateway/src/main/java/br/com/caelum/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/caelum/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,11 @@ package br.com.caelum.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableZuulProxy
 @SpringBootApplication
