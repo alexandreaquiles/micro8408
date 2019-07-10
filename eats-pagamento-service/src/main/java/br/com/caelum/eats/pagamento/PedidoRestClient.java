f335d9376e085c13c017a476873d24845a5ebe4d
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 15:54:30 -0300

    Exercício 8.4, 8.5 e 8.6 Executando segunda instância do monolito, Client Side Load Balancing com Feign, Client Side Load Balancing com Feign no API Gateway

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(url="${configuracao.pedido.service.url}", name="pedido")
+@FeignClient("monolito")
 public interface PedidoRestClient {
 
 	@PutMapping("/pedidos/{pedidoId}/pago")
