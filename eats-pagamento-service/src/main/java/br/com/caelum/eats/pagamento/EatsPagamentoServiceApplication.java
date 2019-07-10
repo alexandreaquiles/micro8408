0e96380a495b804c45cb491f15ec236012376063
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-10 16:57:13 -0300

    Exercício 9.3 Self Registration de pagamentos

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.caelum.eats.pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class EatsPagamentoServiceApplication {
